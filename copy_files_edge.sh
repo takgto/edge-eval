43ae5dbc268c376587ff95e8e2571689ab343963
TGoto <takgoto@g.ecc.u-tokyo.ac.jp>
2023-07-03 16:10:35 +0900

    minor changes.

--- copy_files_edge.sh
+++ copy_files_edge.sh
@@ -1,9 +1,7 @@
-scp init260.sh root@192.168.1.100:/home/root
-scp aliases root@192.168.1.100:/home/root
-scp -r data root@192.168.1.100:/home/root
-scp cpp/* root@192.168.1.100:/home/root/Vitis-AI/examples/Vitis-AI-Library/samples/yolov3
-scp -r video root@192.168.1.100:/home/root/Vitis-AI/examples/Vitis-AI-Library/samples/yolov3
-scp -r mAP root@192.168.1.100:/home/root/Vitis-AI/examples/Vitis-AI-Library/samples/yolov3
-scp -r dpu_yolov3 root@192.168.1.100:/usr/share/vitis_ai_library/models
-#mkdir -p /home/root/Vitis-AI/examples/VART/yolov3
-scp dpu_yolov3/dpu_yolov3.xmodel root@192.168.1.100:/home/root/Vitis-AI/examples/VART/yolov3
+sshpass -p "root" scp init260.sh root@192.168.1.100:/home/root
+sshpass -p "root" scp aliases root@192.168.1.100:/home/root
+sshpass -p "root" scp -r data root@192.168.1.100:/home/root
+sshpass -p "root" scp cpp/* root@192.168.1.100:/home/root/Vitis-AI/examples/Vitis-AI-Library/samples/yolov3
+sshpass -p "root" scp -r video root@192.168.1.100:/home/root/Vitis-AI/examples/Vitis-AI-Library/samples/yolov3
+sshpass -p "root" scp -r mAP root@192.168.1.100:/home/root/Vitis-AI/examples/Vitis-AI-Library/samples/yolov3
+sshpass -p "root" scp -r dpu_yolov3 root@192.168.1.100:/usr/share/vitis_ai_library/models
